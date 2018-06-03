e2305a93d3ce7a40dc07411f726c0e04fc949608
Extruded <extrude57@gmail.com>
2018-06-03 04:47:20 -0700

    Portfolio Challenge
    Linking Dynamically with a portfolio items page.

--- src/components/Expense.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Switch, BrowserRouter,NavLink, Link, Route} from 'react-router-dom';
-const Expense = () => (
-    <div className="dashColor2">
-    This is expense Page
-    </div>
-);
-export default Expense;
